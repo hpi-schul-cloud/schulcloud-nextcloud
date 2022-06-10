435dbd40297c7a0f5a8a33717a61da22d1eb6480
agnisa <arne.gnisa@capgemini.com>
2022-06-10 15:58:12 +0200

    wip

--- collabora-onlyoffice/set_configuration.sh
+++ collabora-onlyoffice/set_configuration.sh
@@ -12,6 +12,7 @@ fi
 rm trusted_domain.tmp
 
 docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
+docker exec -u www-data app-server php occ --no-warnings app:install richdocuments
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
