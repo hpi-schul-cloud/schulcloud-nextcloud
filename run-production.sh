c6407a03685dc321716626f43f5baddb94eea13c
Anastasiia Korzhylova <anastasiia.korzhylova@capgemini.com>
2026-07-29 09:10:56 +0200

    correct GH organization

--- run-production.sh
+++ run-production.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build --target production -t ghcr.io/dBildungsplattform/schulcloud-nextcloud .
+docker build --target production -t ghcr.io/hpi-schul-cloud/schulcloud-nextcloud .
 
 helm uninstall schulcloud-nextcloud || true
 
